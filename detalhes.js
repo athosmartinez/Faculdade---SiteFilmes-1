523ccfd59d26d332ae79dd9ec7d1bf5b4e53f4b2
Athos Martinez <athosandrade04@gmail.com>
2022-06-20 07:58:48 -0300

    Site filmes

--- detalhes.js
+++ detalhes.js
@@ -1,20 +1,53 @@
 const API_KEY = "4fcc4590f648e11f5b9030e8b5746e6e";
 
+const filmes = [];
+
+window.onload = () => {
+    document.getElementById("Categorias").addEventListener("change", () => carregaDestaqueByCategoria());
+}
+
+function carregaDestaqueByCategoria() {
+    const selected = document.getElementById("Categorias").value;
+    const filter = filmes.filter((f) => f.genre_ids.indexOf(parseInt(selected)) != -1);
+    selected == 0 ? exibeDetalhesComFiltro(filmes) : exibeDetalhesComFiltro(filter);
+}
+
+function exibeDetalhesComFiltro(list) {
+    $("#Detalhes").empty();
+    list.forEach((filme) => {
+        $("#Detalhes").append(
+            `<div class="col-xl-6 col-md-12 col-lg-8 col-sm-12">
+                <div class="card border-white mb-3" style="max-width: 680px;">
+                    <div class="row">
+                        <div>
+                            <img src="https://image.tmdb.org/t/p/w500/${filme.backdrop_path}" class="img-fluid rounded-start" alt="...">
+                        </div>
+                        <div>
+                            <div class="card-body">
+                                <h5 class="card-title">${filme.title}</h5>
+                                <p class="card-text"><b>Sinopse:</b> ${filme.overview}</p>
+                                <p><b>Popularidade:</b> ${filme.popularity}</p>
+                                <p><b>Nº de votos:</b> ${filme.vote_count} - <b>Avaliação:</b> ${filme.vote_average}</p>
+                                <p class="card-text"><small class="text-muted">Data lançamento: ${filme.release_date}</small></p>
+                            
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>`);
+    });
+}
+
 function exibeCategorias() {
-    let divCategorias = document.getElementById("categoriasmain2");
-    let texto = `<select id="Categorias" class="form-select" aria-label="Default select example">
-      <option selected>CATEGORIAS: Todos</option>`;
+    let divCategorias = document.getElementById("Categorias");
+    let texto = `<option selected value="0">CATEGORIAS: Todos</option>`;
   
     let dados = JSON.parse(this.responseText);
   
     for (i = 0; i < dados.genres.length; i++) {
       let categorias = dados.genres[i];
   
-      texto =
-        texto +
-        `
-              <option value="${categorias.id}">${categorias.name}</option>
-              `;
+      texto +=  `<option value="${categorias.id}">${categorias.name}</option>`;
     }
     texto += `</select>`;
     divCategorias.innerHTML = texto;
@@ -40,12 +73,9 @@ function exibeDetalhes() {
     let dados = JSON.parse(this.responseText);
     for (i = 0; i < dados.results.length; i++) {
         let detalhes = dados.results[i];
+        filmes.push(detalhes);
 
-        texto =
-            texto +
-            `
-       
-                <div class="col-xl-6 col-md-12 col-lg-8 col-sm-12">
+        texto +=`<div class="col-xl-6 col-md-12 col-lg-8 col-sm-12">
                     <div class="card border-white mb-3" style="max-width: 680px;">
                         <div class="row">
                             <div class="">
@@ -63,11 +93,7 @@ function exibeDetalhes() {
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>
-        </div>
-        
-            `;
+                </div>`;
     }
 
     divDetalhes.innerHTML = texto;
--- index.js
+++ index.js
@@ -1,141 +1,42 @@
 const API_KEY = "4fcc4590f648e11f5b9030e8b5746e6e";
 
-
-
-
-
-
-//DIVISAO
-
-
-// function exibeCarousel() {
-//   let divCarousel = document.getElementById("carousel");
-//   let texto = ` `;
-
-//   let dados = JSON.parse(this.responseText);
-
-//   for (i = 0; i < 4; i++) {
-//     let carousel1 = dados.results[i]; 
-//     if(carousel1.id != undefined){
-//       xhrCar.open(
-//         "GET",
-//         `https://api.themoviedb.org/3/movie/${carousel1.id}/videos?api_key=${API_KEY}&language=pt-BR, false`
-//       );
-//       xhrCar.send();
-//       console.log(xhrCar.responseText);
-
-//       let dataVideos = JSON.parse(xhrCar.responseText);
-//     }
-
-//     texto =
-//       texto +
-//       `
-//             <div class="carousel-item ${i == 0 ? "active" : ""}">
-//             <div class="row">
-//                 <div class="col-xl-6 col-md-12 col-lg-12">
-//                     <iframe class="videomain1" width="830" height="330" src="https://www.youtube.com/embed/YA_T5kar83Q" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-//                 </div>
-
-//                 <div id="textomain1" class="col-xl-6 col-md-12 col-lg-12">
-//                     <h2>As aventuras de PI</h2>
-//                     <div class="textoPI">
-//                         <p><b>Sinopse:</b> Pi e sua família decidem ir para o Canadá depois de fechar o zoológico da família. A embarcação deles naufraga, e o jovem sobrevive junto com alguns animais, incluindo um temível tigre de Bengala, com
-//                             o qual desenvolve uma ligação.</p>
-//                         <p><b>Diretor:</b> Ang Lee | <b>Roteirista:</b> Yann Martel | <b>Estreia:</b> 2012</p>
-
-//                         <p><b>Elenco:</b> Suraj Sharma | Irrfan Khan | Tabu | Gautam Belur</p>
-//                         <p><b> Se você já assistei deixe sua avaliação:</b></p>
-//                     </div>
-//                     <div class="estrelas">
-//                         <input type="radio" id="cm_star-empty" name="fb" value="" checked/>
-//                         <label for="cm_star-1"><i class="fa"></i></label>
-//                         <input type="radio" id="cm_star-1" name="fb" value="1" />
-//                         <label for="cm_star-2"><i class="fa"></i></label>
-//                         <input type="radio" id="cm_star-2" name="fb" value="2" />
-//                         <label for="cm_star-3"><i class="fa"></i></label>
-//                         <input type="radio" id="cm_star-3" name="fb" value="3" />
-//                         <label for="cm_star-4"><i class="fa"></i></label>
-//                         <input type="radio" id="cm_star-4" name="fb" value="4" />
-//                         <label for="cm_star-5"><i class="fa"></i></label>
-//                         <input type="radio" id="cm_star-5" name="fb" value="5" />
-//                     </div>
-//                 </div>
-//             </div>
-//         </div>
-            
-//            `;
-//   }
-//   texto += ` `;
-//   divCarousel.innerHTML = texto;
-// }
-
 const xhrCar = new XMLHttpRequest();
-xhrCar.onload = teste;
-xhrCar.open( "GET", `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=pt-BR&page=2`);
+xhrCar.onload = carregaLancamentos;
+xhrCar.open( "GET", `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=pt-BR&page=1`);
 xhrCar.send();
 
-async function teste() {
+async function carregaLancamentos() {
   const dados = JSON.parse(this.responseText);
-  let divCarousel = document.getElementById("carousel");
   
   let i=0;
-  let texto = '';
   while(i<3) {
     const filme = dados.results[i];
 
     const urlTrailer = await fetch(`https://api.themoviedb.org/3/movie/${filme.id}/videos?api_key=${API_KEY}&language=pt-BR`)
-    .then((body) => body.json()).then((data) => {  return 'https://www.youtube.com/embed/'+data.results[0].key; });
+    .then((body) => body.json()).then((data) => { return data.results.length>0 ? data.results[0].key : null });
 
-    console.log(urlTrailer);
+    const creditos = await carregaCreditos(filme.id);
+   
 
-    texto += `<div class='carousel-item `+ (i==0 ? 'active': '') + `>
-                <div class="row">
-                    <div class="col-xl-6 col-md-12 col-lg-12">
-                        <iframe class="videomain1" width="830" height="330"
-                            src="${urlTrailer}" title="YouTube video player"
-                            frameborder="0"
-                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                            allowfullscreen></iframe>
-                    </div>
+    $('#filmesLancamento').append(
+      `<div class="carousel-item pb-5 ${i==0 ? 'active' : ''}">
+          <div>
+              <div class="row">
+                <iframe class="col-lg-5 col-md-6 col-12" width="560" height="350" src="https://www.youtube.com/embed/${urlTrailer}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
-                    <div id="textomain1" class="col-xl-6 col-md-12 col-lg-12">
-                        <h2>${filme.title}</h2>
-                        <div class="textoPI">
-                            <p><b>Sinopse:</b>${filme.overview}.</p>
-                            <p><b>Diretor:</b> Ang Lee | <b>Roteirista:</b> Yann Martel | <b>Estreia:</b> ${filme.release_date}
-                            </p>
+                  <div class="col-lg-7 col-md-6 col-12">
+                      <h2 class="my-mb-0 my-3">${filme.title}</h2>
+                      <p><b>Sinopse: </b>${filme.overview}</p>
+                      <p><b>Diretor:</b> ${filterCrew(creditos[0].crew, 'Director')} | <b>Roteirista:</b> ${filterCrew(creditos[0].crew, 'Screenplay')} <p><b>Estreia:</b> ${filme.release_date} </p>
 
-                            <p><b>Elenco:</b> Suraj Sharma | Irrfan Khan | Tabu | Gautam Belur</p>
-                            <p><b> Se você já assistei deixe sua avaliação:</b></p>
-                        </div>
-                        <div class="estrelas">
-                            <input type="radio" id="cm_star-empty" name="fb" value="" checked />
-                            <label for="cm_star-1"><i class="fa"></i></label>
-                            <input type="radio" id="cm_star-1" name="fb" value="1" />
-                            <label for="cm_star-2"><i class="fa"></i></label>
-                            <input type="radio" id="cm_star-2" name="fb" value="2" />
-                            <label for="cm_star-3"><i class="fa"></i></label>
-                            <input type="radio" id="cm_star-3" name="fb" value="3" />
-                            <label for="cm_star-4"><i class="fa"></i></label>
-                            <input type="radio" id="cm_star-4" name="fb" value="4" />
-                            <label for="cm_star-5"><i class="fa"></i></label>
-                            <input type="radio" id="cm_star-5" name="fb" value="5" />
-                        </div>
-                    </div>
-                </div>
-            </div>`;
+                      <p><b>Elenco:</b> ${filterCast(creditos[0].cast)}</p>
+                  </div>
+              </div>
+          </div>
+        </div>`);
     i++;
   }
-  console.log(texto);
-
-  $('#carousel').append(texto);
-
-  // divCarousel.appendChild(texto);
 }
-
-
-
-
 
 //DIVISAO
 
@@ -150,8 +51,7 @@ function exibePopulares() {
 
     texto =
       texto +
-      `
-            <div class="col-xl-3 col-md-6 col-lg-6 col-sm-6">
+      `<div class="col-xl-3 col-md-6 col-lg-6 col-sm-6">
             <div class="card" style="width: 18rem;">
                 <img src="https://image.tmdb.org/t/p/w500/${populares.poster_path}" class="card-img-top">
                 <div class="card-body">
@@ -175,114 +75,7 @@ document.addEventListener("load", exibePopulares);
 
 //DIVISAO
 
-const genres = [
-  {
-    id: 28,
-    name: "Action",
-  },
-  {
-    id: 12,
-    name: "Adventure",
-  },
-  {
-    id: 16,
-    name: "Animation",
-  },
-  {
-    id: 35,
-    name: "Comedy",
-  },
-  {
-    id: 80,
-    name: "Crime",
-  },
-  {
-    id: 99,
-    name: "Documentary",
-  },
-  {
-    id: 18,
-    name: "Drama",
-  },
-  {
-    id: 10751,
-    name: "Family",
-  },
-  {
-    id: 14,
-    name: "Fantasy",
-  },
-  {
-    id: 36,
-    name: "History",
-  },
-  {
-    id: 27,
-    name: "Horror",
-  },
-  {
-    id: 10402,
-    name: "Music",
-  },
-  {
-    id: 9648,
-    name: "Mystery",
-  },
-  {
-    id: 10749,
-    name: "Romance",
-  },
-  {
-    id: 878,
-    name: "Science Fiction",
-  },
-  {
-    id: 10770,
-    name: "TV Movie",
-  },
-  {
-    id: 53,
-    name: "Thriller",
-  },
-  {
-    id: 10752,
-    name: "War",
-  },
-  {
-    id: 37,
-    name: "Western",
-  },
-];
 
-function exibeCategorias() {
-  let divCategorias = document.getElementById("categoriasmain2");
-  let texto = `<select id="Categorias" class="form-select" aria-label="Default select example">
-    <option selected>CATEGORIAS: Todos</option>`;
-
-  let dados = JSON.parse(this.responseText);
-
-  for (i = 0; i < dados.genres.length; i++) {
-    let categorias = dados.genres[i];
-
-    texto =
-      texto +
-      `
-            <option value="${categorias.id}">${categorias.name}</option>
-            `;
-  }
-  texto += `</select>`;
-  divCategorias.innerHTML = texto;
-}
-
-const xhrCat = new XMLHttpRequest();
-xhrCat.onload = exibeCategorias;
-xhrCat.open(
-  "GET",
-  `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}&language=pt-BR`
-);
-xhrCat.send();
-
-document.addEventListener("load", exibeCategorias);
 //DIVISAO
 function exibeAvaliacoes() {
   let divAvaliacoes = document.getElementById("Avaliacoes");
@@ -305,7 +98,7 @@ function exibeAvaliacoes() {
                 <div class="card-body">
                     <h5 class="card-title">${avaliacoes.author}</h5>
                     <div class="overflow-auto">
-                    <a href="${avaliacoes.url}" style="text-decoration:none">
+                    <a href="${avaliacoes.url}" target="_blank" style="text-decoration:none">
                     <p class="card-text">${avaliacoes.content}</p>
                     </a>
                    </div>
@@ -363,3 +156,22 @@ xhrEnt.open(
 xhrEnt.send();
 
 document.addEventListener("load", exibeEntrevistas);
+
+async function carregaCreditos(idFilme) {
+  const credits = [];
+  await $.ajax(`https://api.themoviedb.org/3/movie/${idFilme}/credits?api_key=${API_KEY}&language=pt-BR`)
+  .then((data) => { credits.push(data) });
+  return credits;
+}
+
+function filterCrew(list, filter) {
+  let result = "";
+  list.map((c) => { if(c.job == filter) result += result.length>0 ? ", "+c.name : c.name });
+  return result;
+}
+
+function filterCast(list) {
+  let result = "";
+  list.map((c) => { if(c.order <7) result += result.length>0 ? ", "+c.name : c.name });
+  return result;
+}
